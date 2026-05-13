398b6b16d19c787ec829d253771e3ba85bc1b7fb
Elias Olsson <eliasolsson5@gmail.com>
2026-05-13 21:32:06 +0200

    Fail test för jest

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,12 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('pop removes element from stack', () => {
+
+    stack.push("Liverpool");
+
+    stack.pop();
+
+    expect(stack.peek()).toBe("Liverpool");
+});
